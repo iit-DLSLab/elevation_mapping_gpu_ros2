fe362bd96d3e0ea3143f4ce123b7eaf37bb89048
ylenianistico <ylenianistico95@gmail.com>
2026-05-19 16:11:48 +0200

    updating Dockerfile.orin

--- elevation_mapping_cupy/docker/build.sh
+++ elevation_mapping_cupy/docker/build.sh
@@ -14,9 +14,8 @@ colcon build \
         --cmake-args \
                 "-DCMAKE_BUILD_TYPE=$BUILD_TYPE" \
                 "-DCMAKE_EXPORT_COMPILE_COMMANDS=On" \
-                "-DBUILD_TESTING=OFF"\
-                "-DCMAKE_CXX_FLAGS="-Wl,--allow-shlib-undefined""\
-        -Wall -Wextra -Wpedantic -Wshadow \
+                "-DBUILD_TESTING=OFF" \
+                "-DCMAKE_CXX_FLAGS=-Wl,--allow-shlib-undefined -Wall -Wextra -Wpedantic -Wshadow" \
         --packages-skip \
                 convex_plane_decomposition \
                 convex_plane_decomposition_ros
